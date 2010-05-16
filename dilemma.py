971bb767fad7bc868d97dbdfe83eba0ae0f12c9f
Antti Rasinen <ars@iki.fi>
2010-05-16 10:33:06 +0300

    Separated iteration and scoring

--- dilemma.py
+++ dilemma.py
@@ -51,6 +51,13 @@ def tournament(contestants):
     """
     return itertools.combinations(contestants, 2)
 
+def iterate(rounds, f1, f2):
+    r1, r2 = f1(None), f2(None)
+    yield r1, r2
+    for k in range(rounds - 1):
+        r1 = f1(r2)
+        r2 = f2(r1)
+        yield r1, r2
 
 def main():
     rounds = int(sys.argv[1])
@@ -60,12 +67,13 @@ def main():
     
     for p1, p2 in tournament(CONTESTANTS):
         f1, f2 = CONTESTANTS[p1], CONTESTANTS[p2]
-        reply1, reply2 = f1(None), f2(None)
-        for round in range(rounds - 1):
+        # reply1, reply2 = f1(None), f2(None)
+        for reply1, reply2 in iterate(rounds, f1, f2):
+            # Restrict to strategies that do not use the cost
+            # matrix.
             s1, s2 = scoring[reply1, reply2]
             scores[p1] += s1
             scores[p2] += s2
-            reply1, reply2 = f1(reply2), f2(reply1)
     
     for p in scores:
         print "%16s: %.4f" % (p, scores[p] / rounds)
