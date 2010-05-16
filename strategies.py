de21cc9d2fe61b9f5dc31a270ddf0b35b9a42c72
Antti Rasinen <ars@iki.fi>
2010-05-16 12:01:35 +0300

    added the unforgiving strategy

--- strategies.py
+++ strategies.py
@@ -2,10 +2,19 @@
 
 from decorators import simple
 
-"""Strategy collection for the iterated prisoner's dilemma."""
+"""Strategy collection for the iterated prisoner's dilemma.
+The arguments for a strategy are the opponent's last reply and
+a state object. The strategy returns a reply and a new state object.
+
+If the strategy does not make use of the state, it can use the simple
+decorator. Simple strategies take in the last reply and return their new one."
+"""
 
 COOPERATE = 'cooperate'
 DEFECT = 'defect'
+
+always_defect = simple(lambda prev: DEFECT)
+always_cooperate = simple(lambda prev: COOPERATE)
 
 @simple
 def titfortat(tat):
@@ -22,16 +31,16 @@ def titfortat(tat):
         return COOPERATE
     return tat
 
-@simple
-def always_defect(prev):
-    return DEFECT
-
-@simple
-def always_cooperate(prev):
-    return COOPERATE
-
-
+def unforgiving(prev, defect_counter=0):
+    """Start out trusting, but if other defects, punish for 10 turns"""
+    if defect_counter:
+        return DEFECT, defect_counter - 1
     
+    if prev == DEFECT:
+        return DEFECT, 10
+    
+    return COOPERATE, 0
+
 
 if __name__ == '__main__':
     import doctest
