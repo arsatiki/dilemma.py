f061938daaecd02a5f206b16566a8f971309f6ca
Antti Rasinen <ars@iki.fi>
2010-05-16 07:52:40 +0300

    added strategies

--- dilemma.py
+++ dilemma.py
@@ -1,5 +1,13 @@
 # encoding: utf-8
 import sys
+import itertools
+
+def tournament(contestants):
+    """Returns an all against all sequence of player pairs.
+    tournament
+    """
+    return itertools.combinations(contestants, 2)
+
 
 def main():
     pass
---
+++ strategies.py
@@ -0,0 +1,24 @@
+# encoding: utf-8
+
+"""Strategy collection for the iterated prisoner's dilemma."""
+
+COOPERATE = 'cooperate'
+DEFECT = 'defect'
+
+def titfortat(tat):
+    """Returns the same answer given by the opponent during the previous
+       iteration.
+       >>> titfortat(None) == COOPERATE
+       True
+       >>> titfortat(COOPERATE) == COOPERATE
+       True
+       >>> titfortat(DEFECT) == DEFECT
+       True
+    """
+    if tat is None:
+        return COOPERATE
+    return tat
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
